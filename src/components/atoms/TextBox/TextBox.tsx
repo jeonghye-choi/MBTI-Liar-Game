c3c6b5d9d6fec62deb3ec23a2060ee2898cac917
ChoiJuyoung <cjy020718@gmail.com>
2022-08-14 05:12:43 +0900

    feat: 텍스트 박스 스타일

--- src/components/atoms/TextBox/TextBox.tsx
+++ src/components/atoms/TextBox/TextBox.tsx
@@ -1,27 +1,18 @@
 import * as React from "react";
-import "./styles";
+import "./styles.css";
 import classnames from "classnames";
 
 interface Props {
-  backgroundColor?: string;
-  color?: string;
-  fontSize?: number;
-  fontWeight?: number;
-  children: string;
+  children: React.ReactNode;
 }
 
 function TextBox(props: Props) {
-  const { backgroundColor, color, fontSize, fontWeight } = props;
+  const { children } = props;
 
   return (
-    <div
-      className={classnames(
-        "styled-textbox",
-        `styled-textbox-background-${backgroundColor}`,
-      )}
-    >
-      {props.children}
-    </div>
+    <div className="text-box">
+      {children}
+    </div>   
   );
 }
 
