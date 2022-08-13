c202464c325a0cff0aab76786b8c432e7e58b625
jeonghye-choi <ppllhm.0@gmail.com>
2022-08-14 06:25:00 +0900

    feat: add global styles
    #42

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Global, ThemeProvider } from "@emotion/react";
 import theme from "./assets/theme";
+import styles from "./styles";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
+    <Global styles={styles} />
     <App />
   </ThemeProvider>,
   document.getElementById("root"),
