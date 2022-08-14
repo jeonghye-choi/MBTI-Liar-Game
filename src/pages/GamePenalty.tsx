413360b8992d25315e036156caacf998ccd2474e
jeonghye-choi <ppllhm.0@gmail.com>
2022-08-14 10:10:49 +0900

    feat: add 패널티 입력하는 페이지
    #3

--- src/pages/GamePenalty.tsx
+++ src/pages/GamePenalty.tsx
@@ -1,14 +1,30 @@
 import * as React from "react";
 import { useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import GameLayout from "../components/templates/GameLayout";
 
 function GamePenalty() {
+  let navigate = useNavigate();
+
+  const onCompleteClick = () => {
+    navigate(`/games/room/${dummy_gameroomId}/start`);
+  };
+
+  const dummy_gameroomId = "abcd";
+
   useEffect(() => {}, []);
 
   return (
-    <>
-      GamePenalty Page
-      <div>벌칙을 입력해 주세요</div>
-    </>
+    <GameLayout
+      header="back"
+      contents={
+        <>
+          <h3>벌칙을 입력해 주세요</h3>
+          <input type="text" />
+        </>
+      }
+      bottoms={<button onClick={onCompleteClick}>완료</button>}
+    />
   );
 }
 
