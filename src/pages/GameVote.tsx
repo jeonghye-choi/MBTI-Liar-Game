7f8d5075dcf9a549f6f1b5eceb5ac965a634d179
jeonghye-choi <ppllhm.0@gmail.com>
2022-08-14 10:13:28 +0900

    feat: add 투표하기 페이지
    #6

--- src/pages/GameVote.tsx
+++ src/pages/GameVote.tsx
@@ -1,14 +1,43 @@
 import * as React from "react";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import GameLayout from "../components/templates/GameLayout";
 
 function GameVote() {
+  let navigate = useNavigate();
+
+  const onVoteCompleteClick = () => {
+    navigate(`/games/room/${dummy_gameroomId}/result`);
+  };
+
+  const dummy_gameroomId = "abcd";
+
   useEffect(() => {}, []);
 
   return (
-    <>
-      GameVote Page
-      <div>투표하기</div>
-    </>
+    <GameLayout
+      header="none"
+      contents={
+        <>
+          <h3>투표를 해주세요</h3>
+          <ul className="member-list">
+            <li className="profile-box">
+              <span>profile</span>
+              <span>참가자2</span>
+            </li>
+            <li className="profile-box">
+              <span>profile</span>
+              <span>참가자3</span>
+            </li>
+            <li className="profile-box">
+              <span>profile</span>
+              <span>참가자4</span>
+            </li>
+          </ul>
+        </>
+      }
+      bottoms={<button onClick={onVoteCompleteClick}>투표 완료</button>}
+    />
   );
 }
 
