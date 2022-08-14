af55cf42b55ea0ed38abd8385a1adbca91378852
jeonghye-choi <ppllhm.0@gmail.com>
2022-08-14 10:06:21 +0900

    feat: add Main page layout
    #2

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,28 @@
 import * as React from "react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import useGameRoomActions from "../hooks/useGameRoomActions";
 
 function Main() {
+  let navigate = useNavigate();
+  const gameRoomActions = useGameRoomActions();
+
+  const onMakeGameRoomClick = () => {
+    gameRoomActions.makeGameRoom();
+  };
+  const onFindGameRoomClick = () => {
+    navigate("/games/intro-roomId");
+  };
+
   useEffect(() => {}, []);
 
-  return <>Main Page, 방 만들기</>;
+  return (
+    <>
+      <button onClick={onMakeGameRoomClick}>방 만들기</button>
+      <button onClick={onFindGameRoomClick}>방 찾기</button>
+    </>
+  );
 }
 
 export default Main;
